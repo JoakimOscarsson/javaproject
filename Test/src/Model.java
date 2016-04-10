1cc338a27156cbd23541581b41d2931e918b393d
Joakim Oscarsson <oscarsso@kth.se>
2016-04-10 15:22:38 +0200

    Fixed movement + bouncing

--- Test/src/Model.java
+++ Test/src/Model.java
@@ -9,30 +9,31 @@ import java.awt.Color;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
-public class Model  implements Runnable, ActionListener {
+public class Model  implements Runnable{
 	private Thread engine;
 	private boolean running = false;
 	private int delay = 33;
 	private int fps;
-	private ConcurrentHashMap<String,Drawable> things = new ConcurrentHashMap<String,Drawable>();
+	private ConcurrentHashMap<String,DynamicEntity> things = new ConcurrentHashMap<String,DynamicEntity>(1000);
+	
+	//temporary solution:
+	int[] boudary = {800,640};
 	
 	
 	public Model(){
 
 	}
-	
-	private void calculateMovement(Drawable obj){
 		
-	}
-	
+		
 	public void run(){
 		long lastDataFrameTime = System.nanoTime();
 		int dataFrames = 0;
 		
 		while(this.running){
 			//Move objects
-			for(Drawable i : things.values() ){
+			for(DynamicEntity i : things.values() ){
 				calculateMovement(i);
+				i.update();
 			}
 			
 			
@@ -66,29 +67,29 @@ public class Model  implements Runnable, ActionListener {
 		return out;
 	}
 	 
-	public ConcurrentHashMap<String,Drawable> getThings(){
+	public ConcurrentHashMap<String,DynamicEntity> getThings(){
 		return this.things;
 	}
 	
 	
-	public void addThing(Drawable thing) {
+	public void addThing(DynamicEntity thing) {
 		things.put(thing.getID(), thing);
 	}
 
 
 	public void addRandomThing(int x, int y) {
 		Random rand = new Random();
-		Drawable thing;
+		DynamicEntity thing;
 		String newID = Integer.toString(getHashSize()+1);
 		
 		//int shape = rand.nextInt(4);
 		int shape = rand.nextInt(3);
-		int size = 30 + rand.nextInt(470);
+		int size = 30 + rand.nextInt(120);
 		int R = rand.nextInt(255);
 		int G = rand.nextInt(255);
 		int B = rand.nextInt(255);
 		
-		shape = 0;
+		//shape = 0;
 		
 		switch (shape) {
 			case 0:
@@ -101,7 +102,7 @@ public class Model  implements Runnable, ActionListener {
 				thing = new Polygon(newID, x, y, size, new Color(R, G, B));
 				break;
 			/*case 3:
-				thing = new Square(x, y, size, new Color(R, G, B));
+				thing = new Square(newID, x, y, size, new Color(R, G, B));
 				break;*/
 			default:
 				thing = new Polygon(newID, x, y, size, new Color(R, G, B));
@@ -112,6 +113,8 @@ public class Model  implements Runnable, ActionListener {
 		//System.out.println(things.toString());
 	}
 	
+	
+	
 	public void clearList() {
 		this.things.clear();
 	}
@@ -120,14 +123,42 @@ public class Model  implements Runnable, ActionListener {
 		return this.things.size();
 	}
 	
+	private void calculateMovement(DynamicEntity obj){
+		double angle = obj.getAngle();
+		System.out.println("inc. anlge= "+angle);
+		int speed = obj.getSpeed();
+		
+		//calculate deltas
+		int dx = (int)(speed*Math.cos(angle));
+		int dy = (int)(speed*Math.sin(angle));
+		
+		//Check X boundaries:
+		if((obj.getX()-obj.getR()/2+dx <= 0) || (obj.getX()+obj.getR()/2+dx >= this.boudary[0])){
+			//fix angle:
+			angle = (angle-2*angle+Math.PI)%(2*Math.PI);
+			obj.setAngle(angle);
+			System.out.println("stuck in X");
+		}else{
+			//Move in X
+			obj.updateX(dx);
+		}
+		
+		//Check Y boundaries:
+		if((obj.getY()-obj.getR()/2+dy <= 0) || (obj.getY()+obj.getR()/2+dy >= this.boudary[1])){
+			//fix angle:
+			double tmp = angle;
+			angle = (Math.PI-angle+Math.PI)%(2*Math.PI);
+			obj.setAngle(angle);
+			System.out.println("stuck in Y");
+			System.out.println(Math.toDegrees(tmp) + "->" + Math.toDegrees(angle));
 
-	
-	
-	public void actionPerformed(ActionEvent e){
-		//for(Drawable i:this.things) {
-			//i.update();
-		//}
+		}else{
+			//Move in Y
+			System.out.println("nope!");
+			obj.updateY(dy);
+		}
 	}
+
 }
 
 
